c9e8f9e3688d1688940a73d09719c2c8572dcfb3
azozs <azozsahb99@gmail.com>
2023-02-14 12:35:08 +0300

    --rewrite data generation

--- gen.py
+++ gen.py
@@ -1,5 +1,4 @@
-""" This script generates train sets from several building data
-"""
+""" This script generates train sets from several building data"""
 from __future__ import print_function, division
 
 import json
@@ -12,6 +11,8 @@ import numpy as np
 from nilmtk import DataSet
 from nilmtk.electric import align_two_meters
 
+from utils.device_class import get_device_conf
+
 ukdale_windows = [
     ("2014-9-1", "2014-9-30"),
     ("2013-9-1", "2013-9-30"),
@@ -19,13 +20,13 @@ ukdale_windows = [
     ("2013-4-7", "2013-5-7"),
     ("2014-9-1", "2014-9-30"),
 ]
+key_names = ['fridge', 'microwave', 'dish_washer', 'kettle', 'washing_machine']
 
 train_size = 200000
 
 
 def create_trainset(meter, mains, train_size, window_size):
-    """Creates a time series from the raw UKDALE DataSet
-    """
+    """Creates a time series from the raw UKDALE DataSet """
     all_x_train = np.empty((train_size, window_size, 1))
     all_y_train = np.empty((train_size,))
     low_index = 0
@@ -35,7 +36,7 @@ def create_trainset(meter, mains, train_size, window_size):
         if chunk.shape[0] < 3000:
             continue
         chunk.fillna(method='ffill', inplace=True)
-        X_batch, Y_batch = gen_batch(chunk.iloc[:, 1], chunk.iloc[:, 0], chunk.shape[0] - window_size, 0, window_size)
+        X_batch, Y_batch = generate_batch(chunk.iloc[:, 1], chunk.iloc[:, 0], chunk.shape[0] - window_size, 0, window_size)
         high_index = min(len(X_batch), train_size - low_index)
         all_x_train[low_index:high_index + low_index] = X_batch[:high_index]
         all_y_train[low_index:high_index + low_index] = Y_batch[:high_index]
@@ -46,43 +47,45 @@ def create_trainset(meter, mains, train_size, window_size):
     return all_x_train, all_y_train
 
 
-def gen_batch(mainchunk, meterchunk, batch_size, index, window_size):
+def generate_batch(mainchunk, meterchunk, batch_size, index, window_size):
     """Generates batches from dataset
 
     Parameters
     ----------
     index : the index of the batch
     """
-    w = window_size
     offset = index * batch_size
-    X_batch = np.array([mainchunk[i + offset:i + offset + w]
-                        for i in range(batch_size)])
+    x_batch_list = []
+    for i in range(batch_size):
+        x_batch_list.append(mainchunk[i + offset:i + offset + window_size])
+    x_batch = np.array(x_batch_list)
 
-    Y_batch = meterchunk[w - 1 + offset:w - 1 + offset + batch_size]
-    X_batch = np.reshape(X_batch, (len(X_batch), w, 1))
+    y_batch = meterchunk[window_size - 1 + offset:window_size - 1 + offset + batch_size]
+    x_batch = np.reshape(x_batch, (len(x_batch), window_size, 1))
 
-    return X_batch, Y_batch
+    return x_batch, y_batch
 
 
-def opends(building, meter):
+def get_test_data(building_number, meter_key):
     """Opens dataset of synthetic data from Neural NILM
 
     Parameters
     ----------
-    building : The integer id of the building
-    meter : The string key of the meter
-
+    building_number : The id of the building
+    meter_key : The db key of the meter
     """
 
     path = "dataset/ground_truth_and_mains/"
-    main_filename = "{}building_{}_mains.csv".format(path, building)
-    meter_filename = "{}building_{}_{}.csv".format(path, building, meter)
-    mains = np.genfromtxt(main_filename)
-    meter = np.genfromtxt(meter_filename)
-    mains = mains
-    meter = meter
-    up_limit = min(len(mains), len(meter))
-    return mains[:up_limit], meter[:up_limit]
+    main_filename = "{}building_{}_mains.csv".format(path, building_number)
+    meter_filename = "{}building_{}_{}.csv".format(path, building_number, meter_key)
+    mains_reading = np.genfromtxt(main_filename)
+    meter_reading = np.genfromtxt(meter_filename)
+    if len(mains_reading) != len(meter_reading):
+        min_length = min(len(meter_reading), len(meter_reading))
+        mains_reading = mains_reading[:min_length]
+        meter_reading = meter_reading[:min_length]
+
+    return mains_reading, meter_reading
 
 
 def download_dataset():
@@ -105,34 +108,34 @@ if __name__ == "__main__":
         print("Usage: python gen.py ukdale_path")
         exit()
 
-    conf_files = os.listdir("appconf")
-    ds = DataSet(sys.argv[1])
-    for app in conf_files:
-        filename = "appconf/{}".format(app)
-        with open(filename) as data_file:
-            conf = json.load(data_file)
+    # get the test dataset if it is not there
+    if not os.path.exists("dataset"):
+        download_dataset()
 
-        if not os.path.exists("dataset"):
-            download_dataset()
-        os.makedirs(conf['save_path'])
+    dataset = DataSet(sys.argv[1])
+    for device in key_names:
+        device_config = get_device_conf(device)
+        try:
+            os.makedirs(device_config.get_save_path())
+        except Exception as e:
+            print(e)
 
         # Create trainset for meter
-        print(conf["nilmtk_key"])
-        house_keys = conf['train_buildings']
-        window_size = conf['lookback']
+        print(device_config.device_name)
+        house_keys = device_config.train_buildings
+        window_size = device_config.window_size
         all_x_train = np.empty((train_size * len(house_keys), window_size, 1))
-        all_y_train = np.empty((train_size * len(house_keys),))
-        for i, building in enumerate(house_keys):
-            ds.set_window(start=(ukdale_windows[building - 1])[0], end=(ukdale_windows[building - 1])[1])
-            elec = ds.buildings[building].elec
-            meter = elec[conf["nilmtk_key"]]
-            mains = elec.mains()
+        all_y_train = np.empty(train_size * len(house_keys))
+        for index, building in enumerate(house_keys):
+            dataset.set_window(start=(ukdale_windows[building - 1])[0], end=(ukdale_windows[building - 1])[1])
+            electric_meter = dataset.buildings[building].elec
+            meter = electric_meter[device_config.device_name]
+            mains = electric_meter.mains()
             all_x, all_y = create_trainset(meter, mains, train_size, window_size)
-            all_x = all_x
-            all_y = all_y
+            start_index = index * train_size
+            end_index = (index + 1) * train_size
+            all_x_train[start_index:end_index] = all_x
+            all_y_train[start_index:end_index] = all_y
 
-            all_x_train[i * train_size:(i + 1) * train_size] = all_x
-            all_y_train[i * train_size:(i + 1) * train_size] = all_y
-
-        np.save('dataset/trainsets/X-{}'.format(conf['synth_key']), all_x_train)
-        np.save('dataset/trainsets/Y-{}'.format(conf['synth_key']), all_y_train)
+        np.save('dataset/trainsets/X-{}'.format(device_config.device_db_key), all_x_train)
+        np.save('dataset/trainsets/Y-{}'.format(device_config.device_db_key), all_y_train)
